7a43a670ef0c68913a0eb962648f9a3206f8cd04
Berkant Gunes <gunesberkant96@gmail.com>
2025-05-28 11:01:35 +0300

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="Ornek2.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="txtSayi1" runat="server" ForeColor="Red"></asp:TextBox>
+            <br />
+            <asp:TextBox ID="txtSayi2" runat="server"></asp:TextBox>
+            <br />
+            <asp:Button ID="btnTopla" runat="server" Text="Topla" OnClick="btnTopla_Click" BackColor="#CC0066" />
+            <br />
+            <asp:Button ID="btnCikar" runat="server" Text="Çıkar" OnClick="btnCikar_Click" />
+            <br />
+            <asp:Button ID="btnCarp" runat="server" Text="Çarp" OnClick="btnCarp_Click" />
+            <br />
+            <asp:Button ID="btnBol" runat="server" Text="Böl" OnClick="btnBol_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ornek2
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTopla_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2;
+
+            sayi1 = int.Parse(txtSayi1.Text);
+            sayi2 = int.Parse(txtSayi2.Text);
+
+            Response.Write(sayi1 + sayi2);
+
+        }
+
+        protected void btnCikar_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2;
+
+            sayi1 = int.Parse(txtSayi1.Text);
+            sayi2 = int.Parse(txtSayi2.Text);
+
+            Response.Write(sayi1 - sayi2);
+        }
+
+        protected void btnCarp_Click(object sender, EventArgs e)
+        {
+            int sayi1, sayi2;
+
+            sayi1 = int.Parse(txtSayi1.Text);
+            sayi2 = int.Parse(txtSayi2.Text);
+
+            Response.Write(sayi1 * sayi2);
+        }
+
+        protected void btnBol_Click(object sender, EventArgs e)
+        {
+            double sayi1, sayi2;
+
+            sayi1 = double.Parse(txtSayi1.Text);
+            sayi2 = double.Parse(txtSayi2.Text);
+
+            Response.Write(sayi1 / sayi2);
+        }
+    }
+}
